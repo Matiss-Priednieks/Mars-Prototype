88c3053162954b051b0ccb451d2e13d35529d1ab
matissolus <matiss2000@inbox.lv>
2022-10-28 11:09:32 +0100

    Initial Commit
    Just a sphere with a texture and rudimentary rotation with mouse click

---
+++ Mars.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+
+public class Mars : CSGSphere
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    Vector2 mouseDelta;
+    bool mouseEntered = false;
+    public float rotationSpeed = 0.5f;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Vector2 test;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        //todo: mouse input for rotation
+        if (Input.IsMouseButtonPressed(1) && mouseDelta.Length() > 0.1f)
+        {
+            if (mouseEntered)
+            {
+                RotationDegrees += new Vector3(mouseDelta.y * rotationSpeed, mouseDelta.x * rotationSpeed, 0);
+                //todo: fix
+            }
+        }
+    }
+    private void _on_Area_input_event(object camera, object @event, Vector3 position, Vector3 normal, int shape_idx)
+    {
+        if (@event is InputEventMouseMotion mouseMotion)
+        {
+            mouseDelta = mouseMotion.Relative;
+        }
+    }
+
+    private void _on_Area_mouse_entered()
+    {
+        mouseEntered = true;
+    }
+    private void _on_Area_mouse_exited()
+    {
+        mouseEntered = false;
+    }
+
+
+
+
+
+}
+
+
+
+
+
