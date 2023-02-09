9d7233ff8d35142b840da6ce63628a71ab8be13b
matissolus <matiss2000@inbox.lv>
2023-02-09 15:51:21 +0000

    Revert "Merge branch 'master' into laptop-outdated"
    This reverts commit 5f2f6a3fa7604927027cd518fd916964ac76d889, reversing
    changes made to cff7eb336d529a77793c8c760e8f15918a8e1e9d.

--- PlanetData.cs
+++ PlanetData.cs
@@ -6,13 +6,8 @@ public class PlanetData : Resource
     [Export] float Radius = 3;
     [Export] int Resolution = 5;
     [Export] Texture UVTex;
-    [Export] Texture UVTexHeightMap;
-    [Export] OpenSimplexNoise NoiseMap = new OpenSimplexNoise();
-    [Export(PropertyHint.Range, "0.1f,1,")] float Amplitude = 0;
 
-    public void _Ready()
-    {
-    }
+
     public float GetRadius()
     {
         return Radius;
@@ -25,19 +20,8 @@ public class PlanetData : Resource
     {
         return UVTex;
     }
+    // public Vector3 PointOnPlanet(Vector3 PointOnUnitSphere)
+    // {
 
-    public void SetNoiseMap(OpenSimplexNoise noise)
-    {
-        var noiseMap = noise;
-    }
-    public Vector3 PointOnPlanet(Vector3 PointOnUnitSphere)
-    {
-        var imgX = (int)PointOnUnitSphere.x;
-        var imgY = (int)PointOnUnitSphere.y;
-        var heightColour = UVTexHeightMap.GetData().GetPixel(imgX, imgY);
-        var height = heightColour.s;
-        height = Mathf.Clamp(height, 0, 1);
-
-        return height * Radius * PointOnUnitSphere;
-    }
+    // }
 }
