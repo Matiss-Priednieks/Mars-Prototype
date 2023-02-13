47b97f866091fb32caf84265ddcbcced387ae93e
matissolus <matiss2000@inbox.lv>
2023-02-13 17:08:17 +0000

    w.i.p lowpoly mars collisions

---
+++ assets/planetmarslowerpoly.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class planetmarslowerpoly : Spatial
+{
+    Vector2 mouseDelta;
+    bool mouseEntered = false;
+
+    private void _on_ClickArea_input_event(object camera, object @event, Vector3 position, Vector3 normal, int shape_idx)
+    {
+        if (@event is InputEventMouseMotion mouseMotion)
+        {
+            GD.Print(position);
+            mouseDelta = mouseMotion.Relative;
+        }
+    }
+
+}
