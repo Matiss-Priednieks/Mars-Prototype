2bd4449990645ceb7c79d881bcbc1b8c07cd2bca
matissolus <matiss2000@inbox.lv>
2023-02-28 01:01:30 +0000

    w.i.p missions

--- MissionStation.cs
+++ MissionStation.cs
@@ -3,10 +3,16 @@ using System;
 
 public class MissionStation : StaticBody
 {
-    [Export] MissionResource MissionList;
-
-    public override void _Ready()
+    // [Export] MissionResource MissionList;
+    enum MissionList
     {
-        GetNode<Label3D>("Label3D").Text = MissionList.Missions[0].MissionTitle;
-    }
+        Research,
+        Resource,
+        Recovery
+    };
+
+    // public override void _Ready()
+    // {
+    //     GetNode<Label3D>("Label3D").Text = MissionList.Missions[0].MissionTitle;
+    // }
 }
